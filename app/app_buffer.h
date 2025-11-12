6866005f86940a2e3d41a742fb1eb44504e5bef5
Freezelook <zebozhk@gmail.com>
2025-11-12 13:54:21 +0800

    完成了双缓冲区

---
+++ app/app_buffer.h
@@ -0,0 +1,6 @@
+#ifndef __APP_BUFFER_H__
+#define __APP_BUFFER_H__
+#include "app_common.h"
+
+
+#endif /* __APP_BUFFER_H__ */
---
+++ test/app_buffer_test.c
@@ -0,0 +1,6 @@
+
+#include "app_buffer.h"
+int main(int argc, char const *argv[])
+{
+    return 0;
+}
